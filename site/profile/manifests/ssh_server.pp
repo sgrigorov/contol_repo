84af9bacba07d3f2ab8f159f18f9b1e303cf29b6
sgrigorov <s_grigorov@yahoo.com>
2018-02-11 09:29:33 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCqU0AMu794IaMvJtwvnCxXiGSxjGWBtHW+BbgP0CWPf3GEZqkYwsi9fGgx0fPWuIu6L0adqv/Z85C2KMjHHA1FC6kh84ntYv+2EvXKgkq7+kLHLsMuZwwnUedgeKVVTeIF2vD2f6D9S2GqEuPmOPzyCwhebzd9vJq8744HlP9GEyxgph39RUg5domrQmycrDt70snIFJCrnTxBKYkQxd5MYYnx8G/ZtqYuayh+aBmdeTMsWVmpik9r/ZMwfEqN/BNc25G+k5aACENuGcnbH5KOPvqtdUWhSfQnuHi2dUeyPJthGQrmAdo6MEJx1AQJdbyR/+/p9rAmJDlQ56VCawlx',
 	}  
 }
