25917a7c90102663df0c93ee3f35272a0f6e7330
sgrigorov <s_grigorov@yahoo.com>
2018-02-11 11:01:57 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,1 +1,24 @@
-
+class minecraft {
+  file {'/opt/minecraft':
+    ensure  => directory,
+  }
+  file {'/opt/minecraft/minecraft_server.jar':
+    ensure  => file,
+    source  => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+  }
+  package {'java':
+    ensure  => present,
+  }
+  file {'eula.txt':
+    ensure  => file,
+    content => 'eula=true',
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure  => file,
+    source  => 'puppet:///modules/minecraft/minecraft.service',
+  }
+  service {'minecraft':
+    ensure  => running,
+    enabel  => true,
+  }
+}
